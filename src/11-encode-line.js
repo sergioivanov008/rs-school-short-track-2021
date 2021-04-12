c0ed6e94b98f6cbfb7356a7c90216a01de4a5887
sergioivanov008 <sergioivanov008@gmail.com>
2021-04-12 10:46:56 +0300

    feat: implement function 11-encode-line.js

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,27 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  if (str === '') return '';
+  let nextStr = '';
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      nextStr += str[i];
+    } else {
+      nextStr += `${str[i]}-`;
+    }
+  }
+  nextStr = nextStr.slice(0, nextStr.length - 1);
+  const newArr = nextStr.split('-');
+  let exitStr = '';
+  for (let i = 0; i < newArr.length; i++) {
+    if (newArr[i].length === 1) {
+      exitStr += newArr[i][0];
+    } else {
+      exitStr = exitStr + newArr[i].length + newArr[i][0];
+    }
+  }
+  return exitStr;
 }
 
 module.exports = encodeLine;
