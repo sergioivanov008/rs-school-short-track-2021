25303270316cfefdc200fffb64f2a2a2898b1304
sergioivanov008 <sergioivanov008@gmail.com>
2021-04-12 10:40:26 +0300

    feat: implement function 04-mine-sweeper.js

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,47 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const row = matrix.length + 2;
+  const matrixNew = [];
+  for (let i = 0; i < row; i++) {
+    matrixNew.push([0]);
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    if (i === 0) {
+      matrixNew[0].push(0);
+    }
+    for (let j = 0; j < matrix[i].length; j++) {
+      matrixNew[0].push(0);
+      matrixNew[i + 1].push(matrix[i][j]);
+      matrixNew[matrixNew.length - 1].push(0);
+      if (j === matrix[i].length - 1) {
+        matrixNew[i + 1].push(0);
+      }
+    }
+    if (i === matrix.length - 1) {
+      matrixNew[matrixNew.length - 1].push(0);
+    }
+  }
+  const matrixExit = [];
+  for (let i = 0; i < matrix.length; i++) {
+    matrixExit.push([]);
+  }
+  for (let i = 1; i < matrixNew.length - 1; i++) {
+    for (let j = 1; j < matrixNew[1].length - 1; j++) {
+      let count = 0;
+      if (matrixNew[i - 1][j - 1] === true) count += 1;
+      if (matrixNew[i - 1][j] === true) count += 1;
+      if (matrixNew[i - 1][j + 1] === true) count += 1;
+      if (matrixNew[i][j - 1] === true) count += 1;
+      if (matrixNew[i][j + 1] === true) count += 1;
+      if (matrixNew[i + 1][j - 1] === true) count += 1;
+      if (matrixNew[i + 1][j] === true) count += 1;
+      if (matrixNew[i + 1][j + 1] === true) count += 1;
+      matrixExit[i - 1].push(count);
+    }
+  }
+  return matrixExit;
 }
 
 module.exports = minesweeper;
